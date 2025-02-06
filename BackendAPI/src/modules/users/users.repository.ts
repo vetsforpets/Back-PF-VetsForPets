3e427fb9bf9420507d8e000b682d7b5d565eba5e
David <davidesparzaj22@gmail.com>
2025-02-06 15:11:27 -0500

    refactor(BackendAPI/src/main): update the code from en to spa

--- BackendAPI/src/modules/users/users.repository.ts
+++ BackendAPI/src/modules/users/users.repository.ts
@@ -1,7 +1,7 @@
 import { InjectRepository } from "@nestjs/typeorm";
 import { Users } from "./entity/users.entity";
 import { Repository } from "typeorm";
-import { HttpException, HttpStatus } from "@nestjs/common";
+import { HttpException, HttpStatus, NotFoundException } from "@nestjs/common";
 
 export class UsersRepository {
     constructor( @InjectRepository(Users) private readonly usersRepository: Repository<Users>){}
@@ -14,7 +14,7 @@ export class UsersRepository {
     async getUserByEmail(email: string){
         const userfiltered = await this.usersRepository.findOne({where: {email}})
         if (!userfiltered) {
-            throw new HttpException('El usuario no ha sido encontrado' , HttpStatus.NOT_FOUND)
+            throw new NotFoundException('El usuario no pudo ser encontrado')
         }
         return userfiltered
     }
