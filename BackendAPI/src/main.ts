e80ae768824c8e354242814faadb2437c36b28ad
carlos <carlos.figueira1212@gmail.com>
2025-02-06 21:49:57 -0400

    feature(Global Pipes) Implementation for Validations

--- BackendAPI/src/main.ts
+++ BackendAPI/src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { BadRequestException, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe ({
+    whitelist: true,
+    exceptionFactory: (errors) => {
+      const cleanErrors = errors.map((error) => {
+        return { property: error.property, constraints: error.constraints }
+      })
+      return new BadRequestException({
+        alert:
+        'Errores detectados: ',
+        errors: cleanErrors
+      })
+    }
+  }))
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
