d6254bbe63288bff8e053aad014d7f48846db910
David <davidesparzaj22@gmail.com>
2025-02-09 15:47:14 -0500

    feat(BackendAPI/modules/petshop): add new repository methods and create SignUpPetshopDTO

--- BackendAPI/src/modules/pet-shop/pet-shop.repository.ts
+++ BackendAPI/src/modules/pet-shop/pet-shop.repository.ts
@@ -1,6 +1,9 @@
 import { InjectRepository } from '@nestjs/typeorm';
 import { PetShop } from './entity/pet-shop.entity';
 import { Repository } from 'typeorm';
+import { PetShopDto } from './dto/signUpPetshop.dto';
+import { BadRequestException } from '@nestjs/common';
+import moment from 'moment';
 
 export class PetShopRepository {
   constructor(
@@ -11,5 +14,18 @@ export class PetShopRepository {
   async find() {
     const petshops = await this.petshopRepository.find();
     return petshops
+  }
+
+  async getPetShopByEmail(email: string){
+    const petshopFiltered = await this.petshopRepository.findOne({where: {email}})
+    if (!petshopFiltered) {
+      throw new BadRequestException('No se ha encontrado a esta veterinaria, por favor revise nuevamente')
+    }
+  }
+
+  async save(petshopDto: PetShop): Promise <Omit<PetShop,'id'> | PetShopDto>{
+    petshopDto.createdAt = new Date().toLocaleDateString()
+    const petshopUser = await this.petshopRepository.save(petshopDto)
+    return petshopUser
   }
 }
