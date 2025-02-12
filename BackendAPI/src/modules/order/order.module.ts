cd7fc3ad3ba3d2c2a027e8c281b2967bd1734dcf
David <davidesparzaj22@gmail.com>
2025-02-12 15:48:36 -0500

    feat(BackendAPI/modules/order): add module, controller, service, and repository structure

---
+++ BackendAPI/src/modules/order/order.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { OrderController } from './order.controller';
+import { OrderService } from './order.service';
+import { OrderRepository } from './order.repository';
+
+@Module({
+  controllers: [OrderController],
+  providers: [OrderService, OrderRepository],
+  exports: [OrderService, OrderRepository]
+})
+export class OrderModule {}
