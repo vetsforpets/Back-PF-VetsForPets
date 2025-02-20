9668ab8867e575b865bd4a06a3a6e6854cb0b4f1
David <davidesparzaj22@gmail.com>
2025-02-20 15:29:43 -0500

    chore(Backend/modulespayment): update debugging logic in PaymentController

--- BackendAPI/src/modules/payment/payment.controller.ts
+++ BackendAPI/src/modules/payment/payment.controller.ts
@@ -20,20 +20,16 @@ export class PaymentController {
     @Req() req: RawBodyRequest<Request>,
     @Res() res: Response,
   ) {
-    const sigHeader = req.headers['stripe-signature'];
-    if (!sigHeader) {
-      throw new BadRequestException('Missing stripe-signature header');
-    }
-    const sig = sigHeader.toString();
     let event;
     try {
-      event = this.paymentService.constructStripeEvent(req.rawBody, sig);
+      event = JSON.parse(req.rawBody.toString());
+      if (event.type === 'checkout.session.completed') {
+        const session = event.data.object as any;
+        await this.paymentService.handleCheckoutSessionCompleted(session);
+      }
+      // event = this.paymentService.constructStripeEvent(req.rawBody, sig);
     } catch (error) {
       throw new BadRequestException(`Error en el webhook: ${error.message}`);
-    }
-    if (event.type === 'checkout.session.completed') {
-      const session = event.data.object as any;
-      await this.paymentService.handleCheckoutSessionCompleted(session);
     }
 
     res.status(200).json({ received: true });
