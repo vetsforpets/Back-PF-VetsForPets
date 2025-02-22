b6f891525e7e4bb9db84fabb2e77d1eb47c878fb
David <davidesparzaj22@gmail.com>
2025-02-22 17:39:51 -0500

    fix(BackendAPI/modules/petshop): remove decorator from controller to prevent auth issues

--- BackendAPI/src/modules/pet-shop/pet-shop.controller.ts
+++ BackendAPI/src/modules/pet-shop/pet-shop.controller.ts
@@ -44,7 +44,6 @@ export class PetShopController {
   @ApiResponse({ status: 200, description: 'Veterinaria encontrada' })
   @ApiResponse({ status: 404, description: 'Veterinaria no encontrada' })
   @ApiBearerAuth()
-  @Admin()
   @Roles(Role.PETSHOP)
   @Get(':id')
   async findPetShopById(@Param('id') id: string): Promise<PetShop> {
