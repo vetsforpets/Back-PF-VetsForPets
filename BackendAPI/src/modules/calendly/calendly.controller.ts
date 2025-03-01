4d882d3a9645507a7b39ef58d39217c3e8ccff97
carlos <carlos.figueira1212@gmail.com>
2025-03-01 16:21:13 -0400

    fix(BackendAPI/src/modules/calendly): Replace JwtGuard with ApiBearerAuth Decorator

--- BackendAPI/src/modules/calendly/calendly.controller.ts
+++ BackendAPI/src/modules/calendly/calendly.controller.ts
@@ -1,14 +1,14 @@
 import { Controller, Get, Query, UseGuards, HttpException, HttpStatus } from '@nestjs/common';
-import { JwtAuthGuard } from '../common/guards/auth.guard';
 import { ValidationPipe } from '@nestjs/common'; 
 import { CalendlyApiService } from './clanedly.service';
 import { ListEventsQueryDto } from './dto/list-events-query.dto';
+import { ApiBearerAuth } from '@nestjs/swagger';
 
 @Controller('calendly')
 export class CalendlyController {
   constructor(private readonly calendlyApiService: CalendlyApiService) {}
   
-  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
   @Get('events')
   async getCalendlyEvents(
     @Query(new ValidationPipe({ transform: true })) query: ListEventsQueryDto, 
