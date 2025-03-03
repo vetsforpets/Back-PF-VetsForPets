650457197cd92f7d39a87cbc2dc6b95973587d64
David <davidesparzaj22@gmail.com>
2025-03-03 18:23:38 -0500

    chore(BackendAPI/src/typeorm): update configuration to connect to deployment database

--- BackendAPI/src/config/typeorm.ts
+++ BackendAPI/src/config/typeorm.ts
@@ -7,21 +7,21 @@ dotenvConfig({ path: '.env' })
 
 const config = {
     type: 'postgres',
-    database: 'vetsforpets_db',
-    host: 'localhost',
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
     port: Number(process.env.DB_PORT),
-    username: 'postgres',
-    password: 'admin',
-    // url: process.env.DATABASE_URL,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    url: process.env.DATABASE_URL,
     entities: ['dist/**/*.entity{.ts, .js}'],
     migrations: ['dist/migrations/*{.ts, .js}'],
     autoLoadEntities: true,
     synchronize: true,
     logging: false,
     dropSchema: false,
-    // ssl: {
-    //     rejectUnauthorized: false,
-    // }
+    ssl: {
+        rejectUnauthorized: false,
+    }
 }
 
 export default registerAs('typeorm', () => config)
