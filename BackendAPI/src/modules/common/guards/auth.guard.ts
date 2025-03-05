ed90695e5d3943c152f5ed0d9bf0fbfa7ff0b542
Elias Adad <eliasdev97@gmail.com>
2025-03-04 23:52:22 -0300

    refac(src/modules/common/guards/auth.guard): add a new validation for those users with inactive or banned accounts

--- BackendAPI/src/modules/common/guards/auth.guard.ts
+++ BackendAPI/src/modules/common/guards/auth.guard.ts
@@ -1,23 +1,36 @@
-import { ExecutionContext, ForbiddenException, Injectable } from "@nestjs/common";
+import { ExecutionContext, ForbiddenException, Injectable, UnauthorizedException } from "@nestjs/common";
+import { Reflector } from "@nestjs/core";
 import { AuthGuard } from "@nestjs/passport";
+import { IS_PUBLIC_KEY } from "src/decorators/public-routes/public-routes.decorator";
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
-    constructor() {
+    constructor(private readonly reflector: Reflector) {
         super();
     }
 
-    async canActivate(context: ExecutionContext) {
+    async canActivate(context: ExecutionContext): Promise<boolean> {
 
-        const canActivate = await super.canActivate(context) // se ejecuta el authGuard('jwt')
+        const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+            context.getHandler(),
+            context.getClass(),
+        ]);
 
-        if (!canActivate) return false
+        if (isPublic) return true;
 
+        const canAct = await super.canActivate(context);
+        if (!canAct) return false
 
-        const request = context.switchToHttp().getRequest()
-        const user = request.user;
+        const req = context.switchToHttp().getRequest()
 
-        if (!user?.isActive) throw new ForbiddenException("Tu cuenta está inactiva o no existe. Contacta al soporte.")
+        const user = req.user;
+
+        if (!user) {
+            throw new UnauthorizedException("Tu cuenta está inactiva o no existe. Contacta al soporte.")
+        }
+        if (!user.isActive) {
+            throw new ForbiddenException("Tu cuenta se encuentra inactiva.")
+        }
 
         return true;
     }
